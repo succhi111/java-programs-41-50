54c9f461bdf338c51b9c57ae2fd2fbdf2ce6e36c
succhi111 <suchendramishra396@gmail.com>
2022-09-09 14:13:04 +0530

    Maximum number of handshakes

---
+++ src/Handshake.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Handshake {
+
+
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter the number");
+            int num = sc.nextInt();
+
+            //handshakes  =  ( (N-1) * N ) / 2
+
+            int total = num * (num-1) / 2;
+
+            System.out.println("For "+ num +" people there will be " +total+" handshakes");
+        }
+    }
